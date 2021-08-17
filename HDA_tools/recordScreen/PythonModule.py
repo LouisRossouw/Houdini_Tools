782f0b9b39d9b439c890ffa3c927580d818ce032
LouisRossouw <80905013+LouisRossouw@users.noreply.github.com>
2021-08-17 20:51:54 +0200

    Delete PythonModule.py

--- HDA_tools/recordScreen/PythonModule.py
+++
@@ -1,5 +0,0 @@
-import toolutils
-
-RecordSC = toolutils.createModuleFromSection('RecordSC', kwargs['type'], 'RecordSC')
-
-Feedback = toolutils.createModuleFromSection('Feedback', kwargs['type'], 'Feedback')
