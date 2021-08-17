89bb9e33736145bf714005e38eaea6aaed352647
LouisRossouw <80905013+LouisRossouw@users.noreply.github.com>
2021-08-17 20:56:15 +0200

    Delete PythonModule.py

--- HDA_tools/PythonModule.py
+++
@@ -1,7 +0,0 @@
-import toolutils
-
-nulls = toolutils.createModuleFromSection('nulls', kwargs['type'], 'nulls')
-
-crane_rig = toolutils.createModuleFromSection('crane_rig', kwargs['type'], 'crane_rig')
-
-Buttons = toolutils.createModuleFromSection('Buttons', kwargs['type'], 'Buttons')
